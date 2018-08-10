62bc5fd2f56c088c2a5715e3effed3ca18a8c9c3
Yanwar Purnama <yanwar.purnama@gmail.com>
2018-08-10 18:09:59 +0700

    Add createdb scripts

--- install.sh
+++ install.sh
@@ -57,6 +57,7 @@ mkdir /scripts
 wget -O /scripts/lscreate $GITRAW/scripts/lscreate
 wget -O /scripts/lsremove $GITRAW/scripts/lsremove
 wget -O /scripts/certbot $GITRAW/scripts/certbot
+wget -O /scripts/createdb $GITRAW/scripts/createdb
 wget -O /usr/bin/lsws $GITRAW/scripts/lsws
 chmod +x /usr/bin/lsws
 chmod +x /scripts/*
